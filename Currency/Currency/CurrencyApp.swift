2dc8bc10ae7336be49fffd12ebd18be3d9de19a1
kzeller02 <kzeller02@ksu.edu>
2023-08-30 18:48:00 -0500

    initial push

---
+++ Currency/Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Kyle Zeller on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
